dbc3219372436e4f08a692fe32a4e74ffe74ba8e
Muhammad Ibad <ibad3572@gmail.com>
2025-02-08 22:43:14 +0500

    Implement prompt chaining with Google Generative AI and dotenv integration

--- Agentic Ai Design Pattrens/02_prompt_chaining_langgraph.py
+++ Agentic Ai Design Pattrens/02_prompt_chaining_langgraph.py
@@ -1,2 +1,25 @@
-from dotenv import load_dotenv, find_dotenv
-from langgraph 
+from dotenv import load_dotenv
+from langgraph.func import entrypoint, task
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+
+load_dotenv()
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash-exp", api_key=os.getenv("GEMINI_API_KEY"))
+
+@task 
+def task1():
+    return model.invoke("What is up?")
+
+@task
+def task2():
+    return model.invoke("Hello there, how are you?")
+
+@entrypoint()
+def main(input):
+    task1_result = task1().result()
+    task2_result = task2().result()
+    print(f"Task 1: {task1_result.content}, Task 2: {task2_result.content}")
+    return f"{task1_result.content} {task2_result.content}"
+
+if __name__ == "__main__":
+    main.invoke(input="HHH")
